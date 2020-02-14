8fcb0196edc207b4f02c2de3fe2e127c1668ee52
robsonsilv4 <robsonsilv410@gmail.com>
2020-02-14 01:52:00 -0300

    feat: adicionado validações do form

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
   final loginController = TextEditingController();
   final senhaController = TextEditingController();
+
+  final focusSenha = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +28,37 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Container _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: <Widget>[
-          _text(
-            'Login',
-            'Digite o seu login',
-            controller: loginController,
-          ),
-          SizedBox(height: 10),
-          _text(
-            'Senha',
-            'Digite a sua senha',
-            password: true,
-            controller: senhaController,
-          ),
-          SizedBox(height: 20),
-          _button("Entrar", _onClickLogin)
-        ],
+  Widget _body() {
+    return Form(
+      key: formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            _text(
+              'Login',
+              'Digite o seu login',
+              controller: loginController,
+              keyboardType: TextInputType.text,
+              textInputAction: TextInputAction.next,
+              nextFocus: focusSenha,
+              validator: _validateLogin,
+            ),
+            SizedBox(height: 10),
+            _text(
+              'Senha',
+              'Digite a sua senha',
+              password: true,
+              controller: senhaController,
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+              focusNode: focusSenha,
+              validator: _validateSenha,
+            ),
+            SizedBox(height: 20),
+            _button("Entrar", _onClickLogin)
+          ],
+        ),
       ),
     );
   }
@@ -43,6 +68,11 @@ class LoginPage extends StatelessWidget {
     String hint, {
     TextEditingController controller,
     bool password = false,
+    TextInputType keyboardType,
+    TextInputAction textInputAction,
+    FocusNode focusNode,
+    FocusNode nextFocus,
+    FormFieldValidator<String> validator,
   }) {
     return TextFormField(
       style: TextStyle(
@@ -60,6 +90,15 @@ class LoginPage extends StatelessWidget {
       ),
       obscureText: password,
       controller: controller,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: (String text) {
+        if (nextFocus != null) {
+          FocusScope.of(context).requestFocus(nextFocus);
+        }
+      },
+      validator: validator,
     );
   }
 
@@ -80,10 +119,40 @@ class LoginPage extends StatelessWidget {
     );
   }
 
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return 'Digite o login';
+    }
+    return null;
+  }
+
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
+      return 'Digite a senha';
+    }
+
+    if (text.length < 3) {
+      return 'A senha precisa ter no mínimo 3 caracteres';
+    }
+    return null;
+  }
+
   void _onClickLogin() {
+    // bool formOk = formKey.currentState.validate();
+
+    if (!formKey.currentState.validate()) {
+      return;
+    }
+
     String login = loginController.text;
     String senha = senhaController.text;
+
     print('Login: $login');
     print('Senha: $senha');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
