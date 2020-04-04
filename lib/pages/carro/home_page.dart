f202b79ea2f38dbf19e13b879c35b4e22858d88c
robsonsilv4 <robsonsilv410@gmail.com>
2020-04-04 15:50:54 -0300

    add: salvo aba favorita

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/prefs.dart';
 import '../../widgets/drawer_list.dart';
 import 'carro_api.dart';
 import 'carros_listview.dart';
@@ -11,38 +12,68 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  _initTabs() async {
+    int tabIndex = await Prefs.getInt('tabIndex');
+
+    _tabController = TabController(length: 3, vsync: this);
+
+    setState(() {
+      _tabController.index = tabIndex;
+    });
+
+    _tabController.addListener(() {
+      Prefs.setInt('tabIndex', _tabController.index);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initTabs();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Carros'),
-          bottom: TabBar(tabs: [
-            Tab(
-              text: 'Clássicos',
-            ),
-            Tab(
-              text: 'Esportivos',
-            ),
-            Tab(
-              text: 'Luxo',
-            ),
-          ]),
-        ),
-        body: TabBarView(children: [
-          CarrosListView(
-            tipo: TipoCarro.classicos,
-          ),
-          CarrosListView(
-            tipo: TipoCarro.esportivos,
-          ),
-          CarrosListView(
-            tipo: TipoCarro.luxo,
-          ),
-        ]),
-        drawer: DrawerList(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Carros'),
+        bottom: _tabController == null
+            ? null
+            : TabBar(
+                controller: _tabController,
+                tabs: [
+                  Tab(
+                    text: 'Clássicos',
+                  ),
+                  Tab(
+                    text: 'Esportivos',
+                  ),
+                  Tab(
+                    text: 'Luxo',
+                  ),
+                ],
+              ),
       ),
+      body: _tabController == null
+          ? null
+          : TabBarView(
+              controller: _tabController,
+              children: [
+                CarrosListView(
+                  tipo: TipoCarro.classicos,
+                ),
+                CarrosListView(
+                  tipo: TipoCarro.esportivos,
+                ),
+                CarrosListView(
+                  tipo: TipoCarro.luxo,
+                ),
+              ],
+            ),
+      drawer: DrawerList(),
     );
   }
 }
